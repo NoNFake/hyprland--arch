b0826db4be4343959a371eb9be0f8ecb3c8e032b
Restored [.-.] <86575196+NoNFake@users.noreply.github.com>
2023-04-05 22:31:31 +0300

    Update hyprland-install.sh

--- hyprland-install.sh
+++ hyprland-install.sh
@@ -1,1 +1,5 @@
-yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info
+#yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info
+
+git clone --recursive https://github.com/hyprwm/Hyprland
+cd Hyprland
+sudo make install
